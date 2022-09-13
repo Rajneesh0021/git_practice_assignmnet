d68b09ea4a24f9f858611a906ac58e0c806ad22b
rajneesh <thedreamerone0021@gmail.com>
2022-09-13 13:18:53 +0530

    here is our first assignment

---
+++ problem.js
@@ -0,0 +1,21 @@
+//palindrome problem
+    let N=5
+    let str='naman'
+   let bag='';
+   let bag2='';
+   for(i=0;i<N;i++){
+       bag=bag+str[i]
+      
+   for(j=N;j>0;j--)
+       bag2=bag2+str[j];
+   }if(bag2===bag){
+       console.log('Yes');
+   }else { console.log('no');}
+   
+  
+   //prime number problem
+  
+   let num=13;
+   if(num%)
+  
+   
